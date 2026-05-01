d9742038c2c5cf48e9a0bb7762664fe1e895281e
mujeebshk <shaikmujeeb273@gmail.com>
2026-05-01 23:15:15 +0530

    Fix deploy issue

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/TailWebs-React/",
   plugins: [react()],
   server: {
     port: 3000,
